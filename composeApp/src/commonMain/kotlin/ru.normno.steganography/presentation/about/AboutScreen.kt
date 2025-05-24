57a211eca352c2385301747028487cc27c62ad91
NORMss <serega.b301@gmail.com>
2025-05-24 23:57:08 +0700

    feat: updated versions of modules

--- composeApp/src/commonMain/kotlin/ru.normno.steganography/presentation/about/AboutScreen.kt
+++ composeApp/src/commonMain/kotlin/ru.normno.steganography/presentation/about/AboutScreen.kt
@@ -1,12 +1,18 @@
 package ru.normno.steganography.presentation.about
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.vectorResource
+import steganography.composeapp.generated.resources.Res
 
 @Composable
 fun AboutScreen(
@@ -18,8 +24,27 @@ fun AboutScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(
-            text = "AboutScreen"
-        )
+        Row(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(0.9f),
+            ) {
+                Text(
+                    text = "Steganography App"
+                )
+                Icon(
+                    imageVector = vectorResource(Res.drawable.github),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .clickable {
+
+                        }
+                )
+            }
+        }
     }
 }
